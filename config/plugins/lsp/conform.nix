b0050ea2203af5b9c2449f5932f672c15431f012
arjun <arjun.salunkhe@proton.me>
2025-03-10 21:00:16 +0530

    fixed conform

--- config/plugins/lsp/conform.nix
+++ config/plugins/lsp/conform.nix
@@ -3,10 +3,6 @@
     enable = true;
     settings = {
 
-      format_on_save = {
-        lspFallback = true;
-        timeoutMs = 500;
-      };
       notify_on_error = true;
 
       formatters_by_ft = {
